5afbc74559c637546a116c1256ee22e0aa35bd0c
Jeremy Lacoude <github@lacoude.fr>
2011-06-27 14:20:01 +0200

    Added homepage.

--- lang/en/Main.lang.php
+++ lang/en/Main.lang.php
@@ -4,4 +4,10 @@ $lines = array('login' => 'Login',
 	       'pass' => 'Password',
 	       'login' => 'Connect',
 	       'register' => 'Create a new account',
-	       'lost' => 'Lost password');
+	       'lost' => 'Lost password',
+	       'homeTitle' => 'Atrexus',
+	       'homeDesc' => '<h2>Welcome</h2>
+<p>Atrexus is a simple strategy game in which you use Action Points to create soldiers and use them to attack other soldiers or capture headquarters. But it comes with a twist: the players from one army can use any soldiers created by other players from the same army.</p>
+<h2>How to get started</h2>
+<p>You can try the game without registering. You will have access to public battlefields and your actions and statistics will be linked to your IP address.
+If you want to be sure to keep those or have access to more battlefields, you will have to create an account.</p>');
--- templates/Main/index.tpl
+++ templates/Main/index.tpl
@@ -1,1 +1,4 @@
-<p>Hi !</p>
+<h1><?=$lang->get('homeTitle')?></h1>
+<div class="textWall">
+<?=$lang->get('homeDesc')?>
+</div>
